a1937901add569658901174a202e1119f7e21681
Lautaro <lautarogalarza_9@outlook.com>
2024-05-14 16:58:54 -0300

    cosas

--- Clases/Clase01/Program.cs
+++ Clases/Clase01/Program.cs
@@ -6,7 +6,7 @@ namespace Clase01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello  World!");
         }
     }
 }
